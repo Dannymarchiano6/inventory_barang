cec1866405f099dca4ef3092a64c0506f107aed4
Dannymarchiano6 <dannymarchiano6@gmail.com>
2024-01-07 09:05:29 +0700

    add tampilan suppliers

---
+++ resources/views/suppliers/index.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.tabler')
+
+@section('content')
+<div class="page-body">
+    @if($suppliers->isEmpty())
+        <x-empty
+            title="No suppliers found"
+            message="Try adjusting your search or filter to find what you're looking for."
+            button_label="{{ __('Add your first Supplier') }}"
+            button_route="{{ route('suppliers.create') }}"
+        />
+    @else
+        <div class="container-xl">
+            @livewire('tables.supplier-table')
+        </div>
+    @endif
+</div>
+@endsection
