9afdacfec762da18f90055c0f1bc945aa7456f3c
adenurpuspita <adenurpuspitasari11@gmail.com>
2024-01-07 21:08:13 +0700

    update route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,23 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\Dashboards\DashboardController;
+use App\Http\Controllers\InvoiceController;
+use App\Http\Controllers\Order\DueOrderController;
+use App\Http\Controllers\Order\OrderCompleteController;
+use App\Http\Controllers\Order\OrderController;
+use App\Http\Controllers\Order\OrderPendingController;
+use App\Http\Controllers\PosController;
+use App\Http\Controllers\Product\ProductController;
+use App\Http\Controllers\Product\ProductExportController;
+use App\Http\Controllers\Product\ProductImportController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Purchase\PurchaseController;
+use App\Http\Controllers\Quotation\QuotationController;
+use App\Http\Controllers\SupplierController;
+use App\Http\Controllers\UnitController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,18 +31,92 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('php/', function () {
+    return phpinfo();
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware(['auth', 'verified'])->group(function () {
 
-Route::middleware('auth')->group(function () {
+    Route::get('dashboard/', [DashboardController::class, 'index'])->name('dashboard');
+
+    // User Management
+    Route::resource('/users', UserController::class); //->except(['show']);
+    Route::put('/user/change-password/{username}', [UserController::class, 'updatePassword'])->name('users.updatePassword');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::get('/profile/settings', [ProfileController::class, 'settings'])->name('profile.settings');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::resource('/quotations', QuotationController::class);
+    Route::resource('/customers', CustomerController::class);
+    Route::resource('/suppliers', SupplierController::class);
+    Route::resource('/categories', CategoryController::class);
+    Route::resource('/units', UnitController::class);
+
+    // Route Products
+    Route::get('products/import/', [ProductImportController::class, 'create'])->name('products.import.view');
+    Route::post('products/import/', [ProductImportController::class, 'store'])->name('products.import.store');
+    Route::get('products/export/', [ProductExportController::class, 'create'])->name('products.export.store');
+    Route::resource('/products', ProductController::class);
+
+    // Route POS
+    Route::get('/pos', [PosController::class, 'index'])->name('pos.index');
+    Route::post('/pos/cart/add', [PosController::class, 'addCartItem'])->name('pos.addCartItem');
+    Route::post('/pos/cart/update/{rowId}', [PosController::class, 'updateCartItem'])->name('pos.updateCartItem');
+    Route::delete('/pos/cart/delete/{rowId}', [PosController::class, 'deleteCartItem'])->name('pos.deleteCartItem');
+
+    //Route::post('/pos/invoice', [PosController::class, 'createInvoice'])->name('pos.createInvoice');
+    Route::post('invoice/create/', [InvoiceController::class, 'create'])->name('invoice.create');
+
+    // Route Orders
+    Route::get('/orders', [OrderController::class, 'index'])->name('orders.index');
+    Route::get('/orders/pending', OrderPendingController::class)->name('orders.pending');
+    Route::get('/orders/complete', OrderCompleteController::class)->name('orders.complete');
+
+    Route::get('/orders/create', [OrderController::class, 'create'])->name('orders.create');
+    Route::post('/orders/store', [OrderController::class, 'store'])->name('orders.store');
+
+    // SHOW ORDER
+    Route::get('/orders/{order}', [OrderController::class, 'show'])->name('orders.show');
+    Route::put('/orders/update/{order}', [OrderController::class, 'update'])->name('orders.update');
+
+    // DUES
+    Route::get('due/orders/', [DueOrderController::class, 'index'])->name('due.index');
+    Route::get('due/order/view/{order}', [DueOrderController::class, 'show'])->name('due.show');
+    Route::get('due/order/edit/{order}', [DueOrderController::class, 'edit'])->name('due.edit');
+    Route::put('due/order/update/{order}', [DueOrderController::class, 'update'])->name('due.update');
+
+    // TODO: Remove from OrderController
+    Route::get('/orders/details/{order_id}/download', [OrderController::class, 'downloadInvoice'])->name('order.downloadInvoice');
+
+
+    // Route Purchases
+    Route::get('/purchases/approved', [PurchaseController::class, 'approvedPurchases'])->name('purchases.approvedPurchases');
+    Route::get('/purchases/report', [PurchaseController::class, 'dailyPurchaseReport'])->name('purchases.dailyPurchaseReport');
+    Route::get('/purchases/report/export', [PurchaseController::class, 'getPurchaseReport'])->name('purchases.getPurchaseReport');
+    Route::post('/purchases/report/export', [PurchaseController::class, 'exportPurchaseReport'])->name('purchases.exportPurchaseReport');
+
+    Route::get('/purchases', [PurchaseController::class, 'index'])->name('purchases.index');
+    Route::get('/purchases/create', [PurchaseController::class, 'create'])->name('purchases.create');
+    Route::post('/purchases', [PurchaseController::class, 'store'])->name('purchases.store');
+
+    //Route::get('/purchases/show/{purchase}', [PurchaseController::class, 'show'])->name('purchases.show');
+    Route::get('/purchases/{purchase}', [PurchaseController::class, 'show'])->name('purchases.show');
+
+    //Route::get('/purchases/edit/{purchase}', [PurchaseController::class, 'edit'])->name('purchases.edit');
+    Route::get('/purchases/{purchase}/edit', [PurchaseController::class, 'edit'])->name('purchases.edit');
+
+    Route::put('/purchases/update/{purchase}', [PurchaseController::class, 'update'])->name('purchases.update');
+    Route::delete('/purchases/delete/{purchase}', [PurchaseController::class, 'destroy'])->name('purchases.delete');
 });
 
 require __DIR__.'/auth.php';
+
+Route::get('test/', function (){
+    return view('test');
+});
